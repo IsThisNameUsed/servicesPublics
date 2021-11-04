10337c46ff6f7f23b41a877e74dbb1b25db1a1aa
Terry LAPIERRE <tlapierredu54@gmail.com>
2021-11-04 10:23:04 +0100

    Premier commit de test avec le début des squelettes

---
+++ app/src/main/java/fr/afpa/servicespublics/ServiceActivity.kt
@@ -0,0 +1,11 @@
+package fr.afpa.servicespublics
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ServiceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_service)
+    }
+}
