6253988401b1d3d694aa4fcd49bd734d3af577a8
isThisNameUsed <haro@netc.fr>
2021-11-06 14:08:47 +0100

    bouton test pour accès au détail d'un vote

--- app/src/main/java/fr/afpa/servicespublics/SearchByAssemblyMemberScreen1_Activity.kt
+++ app/src/main/java/fr/afpa/servicespublics/SearchByAssemblyMemberScreen1_Activity.kt
@@ -14,6 +14,7 @@ class SearchByAssemblyMemberScreen1_Activity: AppCompatActivity() {
         //Setup buttons
         val home_button = findViewById<Button>(R.id.home_button)
         val return_button = findViewById<Button>(R.id.return_button)
+        val test_details_button = findViewById<Button>(R.id.testButton_toDetailsScreen)
 
         home_button.setOnClickListener {
             startActivity(Intent(this, NationalAssembly_Menu_Activity::class.java))
@@ -23,5 +24,8 @@ class SearchByAssemblyMemberScreen1_Activity: AppCompatActivity() {
             startActivity(Intent(this, MainActivity::class.java))
         }
 
+        test_details_button.setOnClickListener {
+            startActivity(Intent(this, SearchByAssemblyMemberInfoVote_Activity::class.java))
+        }
     }
 }
